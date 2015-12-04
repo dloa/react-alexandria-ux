c1cf09e1ac53a4b39ed03b2229f31f95518dbf89
Niv Sardi <xaiki@evilgiggle.com>
2015-12-04 20:21:51 -0300

    extract img from type
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/Alexandria/BuyBox.js
+++ src/components/Alexandria/BuyBox.js
@@ -3,14 +3,14 @@ import styles from './css/pwyw.module.css';
 
 let imgs = {
     play: require('./svg/paywall-play.svg'),
-    dl:   require('./svg/paywall-download.svg'),
+    buy:   require('./svg/paywall-download.svg'),
     pin:  require('./svg/paywall-pin.svg')
 };
 
-var PWIWItem = ({img, type, prices, actions, children}) => (
+var PWIWItem = ({type, prices, actions, children}) => (
     <li className={styles.item}
-        onClick={() => actions.showPWYW(type)}>
-        <img className={styles.buttonSVG} src={img} />
+        onClick={e => actions.showPWYW(type)}>
+        <img className={styles.buttonSVG} src={imgs[type]} />
         <span className="price">
             {prices[type]?(<span>${prices[type]}</span>):null}
             {children}
@@ -31,13 +31,13 @@ export default class BuyBox extends React.Component {
         return (
             <div className="buybox">
                 <ul className={styles.list}>
-                    <PWIWItem img={imgs.play} type={'play'} {...props}>
+                    <PWIWItem type={'play'} {...props}>
                         to Play
                     </PWIWItem>
-                    <PWIWItem img={imgs.dl} type={'buy'} {...props}>
+                    <PWIWItem type={'buy'} {...props}>
                         to Buy
                     </PWIWItem>
-                    <PWIWItem img={imgs.pin} type={'pin'} {...props}>
+                    <PWIWItem type={'pin'} {...props}>
                         Pin to Play
                     </PWIWItem>
                 </ul>
