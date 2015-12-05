a2a1e7674107952cc2170704a7c2e2d6b3f59506
Niv Sardi <xaiki@evilgiggle.com>
2015-12-05 13:50:38 -0300

    get btcusd average too
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/Alexandria/search.js
+++ src/components/Alexandria/search.js
@@ -25,35 +25,69 @@ function fixDataMess(data) {
     return ret;
 }
 
-
-export default (server, term) => (
-    axios.post('http://'+ server +':41289/alexandria/v1/search',{
-        protocol: 'media',
-        'search-on': 'txid',
-        'search-for': term,
-        'search-like': true
-    }).then(res => {
-        let d = res.data;
-        return (d.status === 'success')?d.response[0]:d.response
-    }).then(d => {
-        let media = d['media-data']['alexandria-media'];
-        let info = media.info;
-        let xinfo= info['extra-info'];
-        let formats = Object.keys(xinfo.element1).filter(k => (k !== 'name'));
-
-        console.log (media);
-        return ({
-            mediaInfo: {
-                title: info.title,
-                artist: xinfo.artist,
+let getPrices = (pwyw) => {
+    if (! pwyw) {
+        return  {
+            play: {
+                suggested: 0.0125,
+                min: 0
             },
-            price: info.price,
-            formats: {
-                options: formats,
-                selected: formats[0]
-            },
-            tracks: fixDataMess(xinfo),
-            data: info
-        });
-    })
+            download: {
+                suggested: 1,
+                min: 0
+            }
+        }
+    }
+
+    var pricesArray = pwyw.split(',')
+
+    return {
+        play: {
+            suggested: pricesArray[0]/100,
+            min: pricesArray[1]/100
+        },
+        buy: {
+            suggested: pricesArray[0],
+            min: pricesArray[1]
+        }
+    }
+}
+
+export default (librarydHost, term) => (
+    Promise.all([axios.get('https://api.bitcoinaverage.com/ticker/global/USD/'),
+                 axios.post('http://'+ librarydHost +':41289/alexandria/v1/search', {
+                     protocol: 'media',
+                     'search-on': 'txid',
+                     'search-for': term,
+                     'search-like': true
+                 }).then((res) => {
+                     let d = res.data;
+                     if (d.status === 'success')
+                         return d.response[0]
+                     throw new Error(d.response)
+                 })])
+        .then(results => {
+            let [btcusd, d] = results;
+            console.log (btcusd, d)
+            let media = d['media-data']['alexandria-media'];
+            let info = media.info;
+            let xinfo= info['extra-info'];
+            let formats = Object.keys(xinfo.element1).filter(k => (k !== 'name'));
+
+            console.log (media);
+            return ({
+                btcusd: btcusd.data['24h_avg'],
+                mediaInfo: {
+                    title: info.title,
+                    artist: xinfo.artist,
+                },
+                prices: getPrices(xinfo.pwyw),
+                formats: {
+                    options: formats,
+                    selected: formats[0]
+                },
+                tracks: fixDataMess(xinfo),
+                data: info
+            });
+        })
 );
